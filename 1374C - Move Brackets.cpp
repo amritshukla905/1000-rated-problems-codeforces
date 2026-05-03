c5e36e1243495812696aaf5bca0b89164fdbcd7b
Amrit Shukla <amritshukla519@gmail.com>
2026-05-03 18:42:39 +0530

    Add files via upload

---
+++ 1374C - Move Brackets.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+typedef long long ll;
+typedef pair < int, int > pii;
+typedef pair < ll, ll > pll;
+typedef vector < int > vi;
+typedef vector < ll > vll;
+
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+
+#define all(x)(x).begin(), (x).end()
+#define sz(x)(int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+    #define rrep(i, a, b) for (int i = a; i >= b; i--)
+
+
+        #ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << "\n"
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n;
+    string s;
+    cin>>n>>s;
+    vi v(n);
+    int cnt = 0;
+    rep(i,0,n){
+        if(s[i] == '(') v[i] = 1;
+    }
+    rep(i,0,n){
+        if(v[i] == 1) cnt++;
+        if((v[i] == 0) && (cnt>0)) cnt--;
+    }
+    cout<<cnt<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
