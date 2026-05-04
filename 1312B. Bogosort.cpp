0ffd88b14cfe3884312d9cb79e6fcfcb62cd9746
Amrit Shukla <amritshukla519@gmail.com>
2026-05-04 12:09:23 +0530

    Add files via upload

---
+++ 1312B. Bogosort.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+typedef long long ll;
+typedef pair < int, int > pii;
+typedef pair < ll, ll > pll;
+typedef vector < int > vi;
+typedef vector < ll > vll;
+
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+
+#define all(x)(x).begin(), (x).end()
+#define sz(x)(int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+    #define rrep(i, a, b) for (int i = a; i >= b; i--)
+
+
+        #ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << "\n"
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n;
+    cin>>n;
+    vi v(n);
+    rep(i,0,n)cin>>v[i];
+    sort(v.rbegin(),v.rend());
+    for(auto x : v) cout<<x<<" ";
+    cout<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
